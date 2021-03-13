8d9ddd2fcfcf93eaf176c7158a6e6bbef9023731
Tracy Ray Remitar <traremitar@addu.edu.ph>
2021-03-13 15:58:50 +0800

    PA-3

--- rootFolder/js/my.js
+++ rootFolder/js/my.js
@@ -1,45 +1,34 @@
-function myFunction() {
-    var x = prompt("Month (1-12)");
-    var y = prompt("Start Day (1-7)");
-    mList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    let dates = document.getElementById("dates");
-    if (x > 12 || x < 1 || y > 7 || y < 1){
-        document.getElementById("invalid").innerHTML = "Invalid Input";
+function show(){
+    subtotal = document.getElementById("SubTotal").value;
+    tax = document.getElementById("Tax");
+    total = document.getElementById("Total");
+    amountPaid = document.getElementById("AmountPaid");
+    if (subtotal <= 10000 && subtotal >= 100){
+        fee = (subtotal * 0.12);
+        subtax = parseInt(subtotal) + fee;
+        amountPaid.removeAttribute("disabled");
+        tax.value = fee.toFixed(2);
+        total.value = subtax.toFixed(2);
+        amountPaid.value = subtax.toFixed(2);
+        coins();
     }
     else{
-        document.getElementById("month").innerHTML = mList[x-1];
-        let counter = 1;
-        for (var w = 1; w <= 7; w++){
-            let row = document.createElement("tr");
-            for (var d = 1; d <= 7; d++){
-                let cell = document.createElement("td");
-                let celltext = document.createTextNode(counter);
-                let blankcell = document.createTextNode("");
-                if (counter > days[x-1]){
-                    break;
-                }
-                else{
-                    if (w == 1 && d < y){
-                        cell.appendChild(blankcell);
-                        if (d == 1){
-                            cell.setAttribute("style", "background-color: #F08080; color: white")
-                        }
-                    }
-                    else{
-                        cell.appendChild(celltext);
-                        if (d == 1){
-                            cell.setAttribute("style", "background-color: #F08080; color: white")
-                        }
-                        counter++;
-                    }
-                    row.appendChild(cell);
-                }
 
-            }
-            dates.appendChild(row);
-        }
     }
-
-
+}
+function coins(){
+    change = document.getElementById("Change");
+    amountPaid = document.getElementById("AmountPaid");
+    total = document.getElementById("Total");
+    pay = document.getElementById("Pay")
+    calchange = parseFloat(amountPaid.value)- parseFloat(total.value);
+    change.value = calchange.toFixed(2);
+    if (change.value >= 0){
+        pay.removeAttribute("disabled");
+        pay.style.backgroundColor = "green";
+    }
+    else{
+        pay.setAttribute("disabled", true);
+        pay.style.backgroundColor = "darkred";
+    }
 }
